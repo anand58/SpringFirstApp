0dc8d5a578c517bd172b0cf36c7082cff31d4b5b
Anand Tiwari <anandt.tiwari12@gmail.com>
2019-05-04 23:06:13 +0530

    committing to JPA branch

--- src/main/java/com/example/SpringFirstApp/model/Author.java
+++ src/main/java/com/example/SpringFirstApp/model/Author.java
@@ -42,6 +42,7 @@ public class Author {
                 ", lastName='" + lastName + '\'' +
                 ", books=" + books +
                 '}';
+
     }
 
     @ManyToMany(mappedBy = "authors")
